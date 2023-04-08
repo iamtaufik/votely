95c7c67976df86f5352b2e33cf77b960be3557b7
iamtaufik <muhtopik07@gmail.com>
2023-04-09 04:27:29 +0700

    add some titles

---
+++ src/app/login/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <title>Login</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  );
+};
+
+export default Head;
---
+++ src/app/participant/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Head = () => {
+  return (
+    <>
+      <title>Ikut Voting</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  );
+};
+
+export default Head;
