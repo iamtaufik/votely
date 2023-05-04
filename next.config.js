56a5272b5d6a170271ced17a1143bc6ea9282483
iamtaufik <muhtopik07@gmail.com>
2023-05-04 20:48:39 +0700

    add feature PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   experimental: {
     appDir: true,
   },
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
