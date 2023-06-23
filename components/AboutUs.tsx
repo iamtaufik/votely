e3123951dadcd75e08ebbbe067b8aaf64211c5d7
raffa <raffajogja2019@gmail.com>
2023-06-23 15:51:06 +0700

    Foto Irvan dan Alfa

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,5 +1,7 @@
 import taufik from '@/assets/taufik.png';
 import RapoL from '@/assets/RapoL.png';
+import Alfa from '@/assets/Alfa.png';
+import Irvan from '@/assets/Irvan.png';
 import Image from 'next/image';
 
 const AboutUs = () => {
@@ -8,10 +10,12 @@ const AboutUs = () => {
       <h1 className="text-[#4A1B9D] text-center text-3xl font-bold">Tentang Kami</h1>
       <p className="text-center text-[#3C3C3C] my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corporis iure aut perspiciatis quod unde nesciunt voluptatibus non iste adipisci.</p>
       <div>
-        <div className="flex relative">
+        <div className="flex flex-wrap relative">
           <Image src={taufik} alt="Muhammad Taufik Hidayat" className="max-w-2xl mx-auto " />
           {/* gambar 500x500 */}
           <Image src={RapoL} alt="Raffa Nur Listiawan D E S" className="max-w-2xl mx-auto " />
+          <Image src={Alfa} alt="Alfa Zidan Prasetyo" className="max-w-2xl mx-auto " />
+          <Image src={Irvan} alt="Muhammadiyah Irvan Shandika" className="max-w-2xl mx-auto " />
         </div>
       </div>
     </div>
