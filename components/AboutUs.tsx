4cfe0976b6a352493d6071b79caa2082c62918fb
raffa <raffajogja2019@gmail.com>
2023-06-23 15:05:58 +0700

    test

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,10 +1,5 @@
 const AboutUs=()=>{
-return (
-    <div>
-        <h1>Tentang Kami</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto delectus quidem exercitationem inventore quos tenetur a, asperiores sapiente modi voluptatem eaque dolore, quae voluptatibus neque. Maiores, veritatis ratione non quae quos asperiores natus laboriosam at dicta fugiat in recusandae sint error autem odio ipsa aut explicabo consectetur, est illo porro.</p>
-    </div>
-)
+
 }
 
-export default AboutUs
+export default Ab
