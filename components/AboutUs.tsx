e8ef076ec8d68fe9f3a9e74a93c735f411ec3f9c
irvanshandika <shandikamuhammadirvan@gmail>
2023-06-24 11:59:10 +0700

    Fix position images

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -12,15 +12,15 @@ const AboutUs = () => {
     <div className="my-10">
       <h1 className="text-[#4A1B9D] text-center text-3xl font-bold">Tentang Kami</h1>
       <p className="text-center text-[#3C3C3C] my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corporis iure aut perspiciatis quod unde nesciunt voluptatibus non iste adipisci.</p>
-      <div>
-        <div className="flex flex-wrap relative">
-          <Image src={taufik} alt="Muhammad Taufik Hidayat" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={RapoL} alt="Raffa Nur Listiawan D E S" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={Alfa} alt="Alfa Zidan Prasetyo" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={Irvan} alt="Muhammadiyah Irvan Shandika" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={Naufal} alt="Naufal Richo Giffari Saputra" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={Aryawijaya} alt="Aryawijara" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
-          <Image src={Okta} alt="Aryawijara" className="w-[150px] h-[150px] sm:mt-16 rounded-full bg-[#4A1B9D] mx-auto " />
+      <div className="flex justify-center w-full">
+        <div className="flex items-center flex-wrap gap-8 justify-center relative w-2/3">
+          <Image src={taufik} alt="Muhammad Taufik Hidayat" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={RapoL} alt="Raffa Nur Listiawan D E S" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={Alfa} alt="Alfa Zidan Prasetyo" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={Irvan} alt="Muhammad Irvan Shandika" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={Naufal} alt="Naufal Richo Giffari Saputra" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={Aryawijaya} alt="Aryawijaya" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
+          <Image src={Okta} alt="Fachrudin Okta Rahmawan" className="lg:w-[150px] lg:h-[150px] sm:mt-16 w-[80px] h-[80px] rounded-full bg-[#4A1B9D] mx-auto " />
         </div>
       </div>
     </div>
