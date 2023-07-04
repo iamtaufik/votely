a36d6e70925332ea7f8a7064429fb4a4332f27c4
aryawijaya508 <aryawijaya508@gmail.com>
2023-07-04 12:46:13 +0700

    Penambahan Copyright Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-[#4A1B9D] py-4 ">
       <p className="font-light text-center text-white">Dibuat dengan ❤️ oleh Votely Team</p>
+      <p className="font-light text-center text-white">Copyright &#169; 2023, Votely Team. All Rights Reserved.</p>
     </footer>
   );
 };
